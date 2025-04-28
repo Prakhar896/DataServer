91eb80ec8322baa55f9ba388ac41e970b6793251
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2025-04-29 00:22:14 +0800

    tested an APScheduler application of streaming, it works

--- test2.py
+++ test2.py
@@ -1,3 +1,26 @@
+# from client import CloudFragment
+# from apscheduler.schedulers.background import BackgroundScheduler
+# import random, time, sys
+
+# scheduler = BackgroundScheduler()
+# scheduler.start()
+# def a(data=None):
+#     print("NEW:", data)
+
+# frag1 = CloudFragment(fragmentID="d90507e925844e5ab1cba1537102a060", secret="a123456", url="http://localhost:8250")
+# print(frag1.initStream())
+
+# def w():
+#     frag1.writeWS({"number": random.randint(0, 100)})
+#     print("wrote")
+
+# j1 = scheduler.add_job(frag1.liveStream, args=[a])
+# j2 = scheduler.add_job(w, 'interval', seconds=3)
+
+# time.sleep(30)
+# scheduler.shutdown()
+# sys.exit(0)
+
 from client import CloudFragment
 
 frag1 = CloudFragment(fragmentID="d90507e925844e5ab1cba1537102a060", secret="a123456", url="http://localhost:8250")
